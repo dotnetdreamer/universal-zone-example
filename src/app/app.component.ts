608f8d2185df5e28eb7d674e8def3b3c0bb9950b
Idrees Khan <idrees.khan@vanderlande.com>
2024-08-17 13:06:06 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppSettingService, UniversalZoneModule } from 'ngx-universal-zone';
+
+import { DbModule, DbService, DbServiceConfig, DbServiceType, DbSettingConfig, DbSettingConstant, ITableOptions, SchemaService } from 'ngx-universal-zone/database';
+
+
+export class DbConstant {
+  public static readonly SETTING = DbSettingConstant.SETTING;
+  public static readonly CUSTOMER = 'customer';
+}
+
+export const dbConfig: DbServiceConfig = {
+  dbType: DbServiceType.IndexDd,
+  dbName: 'universal-zone-example',
+  schema: <ITableOptions[]>[
+    { ...DbSettingConfig.schema },  //must needed in order to crate 'setting' table
+    {
+      name: DbConstant.CUSTOMER,
+      columns: [
+        {
+          name: 'email',
+          isPrimaryKey: true,
+          type: 'TEXT',
+        },
+        {
+          name: 'name',
+          type: 'TEXT',
+        }
+      ],
+    },
+  ],
+}
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'universal-zone-example';
+
+  constructor(private mySettingsSvc: MySettingsService, private dbSvc: DbService) {
+    this.dbSvc.dbInitialized$.subscribe(async () => {
+      console.log('db init');
+      //Your app should start from here
+
+      await this.mySettingsSvc.putTest();
+    });
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MySettingsService extends AppSettingService {
+  constructor(private dbSvc: DbService, private schemaSvc: SchemaService) {
+    super(dbSvc, schemaSvc);
+  }
+
+  putTest() {
+    return this.put('test', 'test value');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { dbFactory, DbModule, DbService, DbServiceType, SchemaService } from 'ngx-universal-zone/database';
+import { dbConfig } from './app.component';
+import { UniversalZoneModule } from 'ngx-universal-zone';
+
+
+function initializeDb(schemaSvc: SchemaService) {
+  return async () => {
+    // const platform = await Capacitor.getPlatform();
+    dbConfig.dbType = DbServiceType.IndexDd;
+
+    schemaSvc.init(dbConfig);
+
+    // dbFactory(schemaSvc);
+  };
+}
+
+export function provideDependentModules() {
+  return importProvidersFrom([DbModule.forRoot(), UniversalZoneModule.forRoot()]);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true })
+    , provideRouter(routes)
+    , {
+      provide: APP_INITIALIZER,
+      useFactory: initializeDb,
+      multi: true,
+      deps: [SchemaService]
+    }
+    , provideDependentModules()
+  ]
 };
